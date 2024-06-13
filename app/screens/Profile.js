9d9093701745a6f1737a35b4ec0f41f26cafbabb
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-06-13 16:46:02 -0400

    account

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,11 +1,11 @@
-import { View, StyleSheet, SafeAreaView, Image } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { useStore } from "../store";
 import colors from "../config/colors";
-import AppListItem from "./components/AppListItem";
 import Screen from "./components/Screen";
 import LinkItem from "./components/LinkItem";
 import authApi from "../api/authApi";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppButton from "./components/Forms/AppButton";
 
 export default function Profile({ navigation }) {
 	const { user, setUser } = useStore();
@@ -15,20 +15,90 @@ export default function Profile({ navigation }) {
 	};
 	return (
 		<Screen>
-			<AppListItem
-				title={"My Passphrase:"}
-				subtitle={user.username + "-" + user.password}
-				name="account"
-			/>
-
-			<View style={{ marginTop: 40 }}>
-				<LinkItem
-					title="Logout"
-					source="logout"
-					color={colors.medium}
-					pressHandler={() => {
-						logoutHandler();
+			<View style={styles.card}>
+				<View
+					style={{
+						flexDirection: "row",
+						alignItems: "center",
+						justifyContent: "space-between",
 					}}
+				>
+					<Text style={styles.heading}>Your Passphrase</Text>
+					<TouchableOpacity
+						style={{
+							borderWidth: 1,
+							borderRadius: 5,
+							padding: 5,
+							paddingHorizontal: 10,
+							borderColor: colors.light,
+							alignItems: "center",
+							justifyContent: "center",
+							flexDirection: "row",
+							margin: 5,
+						}}
+						onPress={() => navigation.navigate("EditProfile")}
+					>
+						<MaterialCommunityIcons
+							name="content-copy"
+							size={16}
+							color={colors.medium}
+						/>
+						<Text style={{ fontSize: 11, margin: 2 }}>Copy</Text>
+					</TouchableOpacity>
+				</View>
+				<View style={styles.passphrase}>
+					<Text style={{ color: colors.dark }}>radical-melon1029</Text>
+				</View>
+			</View>
+
+			<View
+				style={{
+					margin: 20,
+					backgroundColor: "#fff",
+					borderColor: colors.light,
+					borderWidth: 1,
+					padding: 20,
+					height: 300,
+				}}
+			>
+				<Text style={styles.heading}>What is a passphrase?</Text>
+				<Text style={{ color: colors.medium, marginVertical: 10 }}>
+					<View style={{ marginVertical: 10 }}>
+						<Text style={{ color: colors.medium, lineHeight: 22 }}>
+							Use your passphrase to retrieve your watch list and
+							recommendations if you log out.
+						</Text>
+					</View>
+					<View style={{ marginVertical: 10 }}>
+						<Text style={{ color: colors.medium, lineHeight: 22 }}>
+							You can share your passphrase with a friend to collaborate on a
+							list together.
+						</Text>
+					</View>
+					<View style={{ marginVertical: 10 }}>
+						<Text style={{ color: colors.medium, lineHeight: 22 }}>
+							Only share the passphrase with people you trust.
+						</Text>
+					</View>
+				</Text>
+			</View>
+
+			<View style={styles.card}>
+				<Text style={styles.heading}>Sign out</Text>
+				<Text
+					style={{
+						color: colors.medium,
+						width: 250,
+						lineHeight: 22,
+						marginVertical: 10,
+					}}
+				>
+					Be sure to save your passphrase before signing out.
+				</Text>
+				<AppButton
+					title="Sign out"
+					color={colors.danger}
+					onPress={logoutHandler}
 				/>
 			</View>
 		</Screen>
@@ -36,6 +106,23 @@ export default function Profile({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+	heading: {
+		fontSize: 18,
+		lineHeight: 28,
+		fontWeight: "bold",
+	},
+	passphrase: {
+		fontSize: 16,
+		marginTop: 10,
+		borderWidth: 1,
+		padding: 10,
+		borderRadius: 10,
+		borderColor: colors.light,
+	},
+	card: {
+		backgroundColor: "#fff",
+		padding: 20,
+	},
 	container: {
 		height: "100%",
 		backgroundColor: "#f9f9f9",
--- app/screens/components/Forms/AppButton.js
+++ app/screens/components/Forms/AppButton.js
@@ -1,11 +1,15 @@
 import { Text, TouchableOpacity } from "react-native";
 
-export default function AppButton({ pressHandler, color = "primary", title }) {
+export default function AppButton({
+	pressHandler,
+	color = "colors.dark",
+	title,
+}) {
 	return (
 		<TouchableOpacity
 			onPress={pressHandler}
 			style={{
-				backgroundColor: "#000",
+				backgroundColor: color,
 				display: "flex",
 				height: 50,
 				marginVertical: 10,
@@ -14,14 +18,13 @@ export default function AppButton({ pressHandler, color = "primary", title }) {
 				flexDirection: "row",
 				justifyContent: "center",
 				alignItems: "center",
-				borderRadius: 40,
+				borderRadius: 10,
 			}}
 		>
 			<Text
 				style={{
 					fontSize: 18,
 					fontWeight: 500,
-					textTransform: "uppercase",
 					fontWeight: "bold",
 					color: "#FFF",
 				}}
