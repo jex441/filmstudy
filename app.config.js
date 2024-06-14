f2f736a9fc7cccb58678bf5de86994810b24d672
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-06-14 13:33:08 -0400

    building successful

--- app.config.js
+++ app.config.js
@@ -2,6 +2,9 @@ module.exports = {
 	name: "filmstudy",
 	owner: "jex441",
 	expo: {
+		ios: {
+			bundleIdentifier: "com.jex441.filmstudy",
+		},
 		extra: {
 			eas: {
 				projectId: "67d2a46b-79cd-4ea4-bfdb-87f227617320",
